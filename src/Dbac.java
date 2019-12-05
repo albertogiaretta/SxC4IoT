f3415e63f19274c8852e8be9956f211088af91d1
Alberto Giaretta <19826806+albertogiaretta@users.noreply.github.com>
2019-12-05 16:07:30 +0100

    Added DBAC stub class

---
+++ src/Dbac.java
@@ -0,0 +1,13 @@
+
+public class Dbac {
+    
+    //STUB METHOD
+    public static boolean canExtractContract(IoTDev inputDevice) {
+        return true;
+    }
+    
+    //STUB METHOD, WE PRETEND WE EXTRACTED THE CONTRACT
+    public static boolean extractContract(IoTDev inputDevice) {
+        return true;
+    }
+}
