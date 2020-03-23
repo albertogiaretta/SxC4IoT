3303efb7a30a791cb71bb0e2d88b0b82e3500b90
albertogiaretta <19826806+albertogiaretta@users.noreply.github.com>
2020-03-23 11:52:21 +0100

    Implemented Message as a serializable MQTT message.

--- src/Message.java
+++ src/Message.java
@@ -1,16 +1,45 @@
+import java.io.Serializable;
 
-public class Message {
-    public IoTDev sender;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+import com.google.gson.Gson;
+
+@SuppressWarnings("serial")
+public class Message implements Serializable {
+    public String sender;
     public String message;
+    public Contract contract;
     
     public Message() {
-        sender = new IoTDev();
+        sender = new String();
         message = new String();
+        contract = new Contract();
     }
     
     public Message(IoTDev inputSender, String inputMessage) {
         this();
-        sender = inputSender;
+        sender = inputSender.toString();
         message = inputMessage;
+        contract = inputSender.getContract();
     }
+    
+    public byte[] serialize() {
+        Gson gson = new Gson();
+        String jsonString = gson.toJson(this);
+
+        return jsonString.getBytes();
+    }
+
+    
+    public void deserialize(MqttMessage inputMessage) {    
+        String jsonString = new String(inputMessage.getPayload());
+        Gson gson = new Gson();
+        
+        sender = gson.fromJson(jsonString, Message.class).sender;
+        message = gson.fromJson(jsonString, Message.class).message;
+        contract = gson.fromJson(jsonString, Message.class).contract;
+        System.out.println("Sender payload is: " + sender.getBytes().length + " bytes");
+        System.out.println("Message payload is: " + message.getBytes().length + " bytes");
+    }
+    
 }
