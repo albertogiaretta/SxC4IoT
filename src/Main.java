945829c28caca3f9a7e68e1e5bc647347493eec5
albertogiaretta <19826806+albertogiaretta@users.noreply.github.com>
2020-07-07 16:05:08 +0200

    Cleaned up main file for running experiments.

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,20 @@
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
-
 import com.opencsv.CSVWriter;
 
-
-
 public class Main {    
-    static String[] records = {"Sent", "Received", "#OfRules", "PayloadSize", "MessageSize", "ContractSize"};
+    static String[] records = {"Sent", "Received", "#OfRules", "PayloadSize", 
+            "MessageSize", "ContractSize"};
+    static String inputPrefix = "files/2rules/";
+    static String outputSuffix = "output/";
     
     static int iterations = 100;
 
     public static void main(String[] args) {
         try {
-            //ExecuteSxC();
-            //ExecuteSxC();
-
+            ExecuteSxC();
             ExecuteNoSxC();
-            ExecuteNoSxC();
-            
-            ExecuteNoFlowCheck();
             ExecuteNoFlowCheck();
         
         } catch (IOException e) {
@@ -29,52 +24,52 @@ public class Main {
     }
     
     public static void ExecuteSxC() throws IOException {
-        //final FogNode fogNode = new FogNode("files/Policy_PFlegal.json");
         final FogNode fogNode = new FogNode();
-        //final IoTDev deviceC = new IoTDev("files/Contract_D5.json", fogNode, "output/WithSxC");
-        //final IoTDev deviceD = new IoTDev("files/Contract_D2.json", fogNode, "output/WithSxC");
-        final IoTDev deviceC = new IoTDev("files/8rules/Contract_D5.json", fogNode, "output/WithSxC");
-        final IoTDev deviceD = new IoTDev("files/8rules/Contract_D2.json", fogNode, "output/WithSxC");
+        final IoTDev deviceC = new IoTDev(inputPrefix + "Contract_D5.json", 
+                fogNode, outputSuffix + "WithSxC");
+        final IoTDev deviceD = new IoTDev(inputPrefix + "Contract_D2.json", 
+                fogNode, outputSuffix + "WithSxC");
         
         fogNode.addDevice(deviceC);
         fogNode.addDevice(deviceD);
         
-        WriteOutput(deviceC, deviceD, "output/WithSxC");
+        WriteOutput(deviceC, deviceD, outputSuffix + "WithSxC");
     }
     
     public static void ExecuteNoSxC() throws IOException {
-        //final FogNode fogNode = new FogNode("files/Policy_PFlegal.json");
         final FogNode fogNode = new FogNode();
-        //final IoTDevNoSxC deviceC2 = new IoTDevNoSxC("files/Contract_D5.json", fogNode, "output/WithoutSxC");
-        //final IoTDevNoSxC deviceD2 = new IoTDevNoSxC("files/Contract_D2.json", fogNode, "output/WithoutSxC");
-        final IoTDevNoSxC deviceC2 = new IoTDevNoSxC("files/8rules/Contract_D5.json", fogNode, "output/WithoutSxC");
-        final IoTDevNoSxC deviceD2 = new IoTDevNoSxC("files/8rules/Contract_D2.json", fogNode, "output/WithoutSxC");
+        final IoTDevNoSxC deviceC2 = new IoTDevNoSxC(inputPrefix 
+                + "Contract_D5.json", fogNode, outputSuffix + "WithoutSxC");
+        final IoTDevNoSxC deviceD2 = new IoTDevNoSxC(inputPrefix 
+                + "Contract_D2.json", fogNode, outputSuffix + "WithoutSxC");
         
         fogNode.addDevice(deviceC2);
         fogNode.addDevice(deviceD2);
         
-        WriteOutput(deviceC2, deviceD2, "output/WithoutSxC");
+        WriteOutput(deviceC2, deviceD2, outputSuffix + "WithoutSxC");
     }
     
     public static void ExecuteNoFlowCheck() throws IOException {
-        //final FogNode fogNode = new FogNode("files/Policy_PFlegal.json");
         final FogNode fogNode = new FogNode();
-        //final IoTDevNoFlowCheck deviceC3 = new IoTDevNoFlowCheck("files/Contract_D5.json", fogNode, "output/WithContractButNoSxC");
-        //final IoTDevNoFlowCheck deviceD3 = new IoTDevNoFlowCheck("files/Contract_D2.json", fogNode, "output/WithContractButNoSxC");
-        final IoTDevNoFlowCheck deviceC3 = new IoTDevNoFlowCheck("files/8rules/Contract_D5.json", fogNode, "output/WithContractButNoSxC");
-        final IoTDevNoFlowCheck deviceD3 = new IoTDevNoFlowCheck("files/8rules/Contract_D2.json", fogNode, "output/WithContractButNoSxC");
+        final IoTDevNoFlowCheck deviceC3 = new IoTDevNoFlowCheck(inputPrefix 
+                + "Contract_D5.json", fogNode, outputSuffix 
+                + "WithContractButNoSxC");
+        final IoTDevNoFlowCheck deviceD3 = new IoTDevNoFlowCheck(inputPrefix 
+                + "Contract_D2.json", fogNode, outputSuffix 
+                + "WithContractButNoSxC");
         
         fogNode.addDevice(deviceC3);
         fogNode.addDevice(deviceD3);
         
-        WriteOutput(deviceC3, deviceD3, "output/WithContractButNoSxC");
+        WriteOutput(deviceC3, deviceD3, outputSuffix + "WithContractButNoSxC");
     }
     
-    public static void WriteOutput(IoTDev inputDev1, IoTDev inputDev2, String inputPath) throws IOException {
+    public static void WriteOutput(IoTDev inputDev1, IoTDev inputDev2, 
+            String inputPath) throws IOException {
         String s = "ab"; // String long 2 chars
         
-        CSVWriter csvWriter = new CSVWriter(new FileWriter("./" + inputPath + "/"
-        + inputDev1.toString() + "_" + inputDev2.toString() 
+        CSVWriter csvWriter = new CSVWriter(new FileWriter("./" + inputPath 
+                + "/" + inputDev1.toString() + "_" + inputDev2.toString() 
                 + ".csv"));
         csvWriter.writeNext(records);
         csvWriter.close();
