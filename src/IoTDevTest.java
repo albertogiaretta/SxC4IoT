31fa05ef81dc580391a63705d73a54885d296326
albertogiaretta <19826806+albertogiaretta@users.noreply.github.com>
2020-07-07 16:05:57 +0200

    Removed old tests that do not apply anymore.

--- src/IoTDevTest.java
+++ src/IoTDevTest.java
@@ -18,27 +18,8 @@ class IoTDevTest {
     Contract contractD2;
     Contract contractD5;
     
-    /*
-    void init() {
-        fogNodePF = new FogNode("files/Policy_PF.json");
-        fogNodePFlegal = new FogNode("files/Policy_PFlegal.json");
-        
-        deviceA = new IoTDev("files/Contract_D1.json", fogNodePF);
-        deviceB = new IoTDev("files/Contract_D2.json", fogNodePF);
-        deviceC = new IoTDev("files/Contract_D5.json", fogNodePFlegal);
-        deviceD = new IoTDev("files/Contract_D2.json", fogNodePFlegal);
-        deviceE = new IoTDev("files/Contract_D5.json");
-        deviceEmpty = new IoTDev();
-        
-        contractIB = new Contract("files/Contract_IB.json");
-        contractD1 = new Contract("files/Contract_D1.json");
-        contractD2 = new Contract("files/Contract_D2.json");
-        contractD5 = new Contract("files/Contract_D5.json");
-    }*/
-    
     @Test
     final void testUpdateContractWithInconsistentContract() {
-        //init();
         deviceE = new IoTDev("files/Contract_D5.json");
         contractD5 = new Contract("files/Contract_D5.json");
         contractIB = new Contract("files/Contract_IB.json");
@@ -67,7 +48,6 @@ class IoTDevTest {
     
     @Test
     final void testUpdateSoftwaretWithInconsistentContract() {
-        //init();
         deviceE = new IoTDev("files/Contract_D5.json");
         contractD5 = new Contract("files/Contract_D5.json");
         contractIB = new Contract("files/Contract_IB.json");
@@ -96,7 +76,6 @@ class IoTDevTest {
     
     @Test
     final void testUpdateContractWithoutFogNode() {
-        //init();
         deviceE = new IoTDev("files/Contract_D5.json");
         contractD5 = new Contract("files/Contract_D5.json");
         contractD1 = new Contract("files/Contract_D1.json");
@@ -112,7 +91,6 @@ class IoTDevTest {
     
     @Test
     final void testUpdateContract() {
-        //init();
         fogNodePFlegal = new FogNode("files/Policy_PFlegal.json");
         deviceC = new IoTDev("files/Contract_D5.json", fogNodePFlegal);
         contractD1 = new Contract("files/Contract_D1.json");
@@ -146,7 +124,6 @@ class IoTDevTest {
     
     @Test
     final void testUpdateSoftware() {
-        //init();
         fogNodePFlegal = new FogNode("files/Policy_PFlegal.json");
         deviceC = new IoTDev("files/Contract_D5.json", fogNodePFlegal);
         contractD1 = new Contract("files/Contract_D1.json");
@@ -180,7 +157,6 @@ class IoTDevTest {
     
     @Test
     final void testPolicyPFNotInserted() {
-        //init();
         fogNodePF = new FogNode("files/Policy_PF.json");
         
         assertEquals(fogNodePF.numberOfContracts(), 0, "Policy is not valid, "
@@ -188,52 +164,7 @@ class IoTDevTest {
     }
     
     @Test
-    final void testMessageFromCD1toCD2() {
-        //init();
-        fogNodePF = new FogNode("files/Policy_PF.json");
-        deviceA = new IoTDev("files/Contract_D1.json", fogNodePF);
-        deviceB = new IoTDev("files/Contract_D2.json", fogNodePF);
-        
-        assertFalse(deviceA.sendMessage(deviceB, "Hello there!"), "There is an "
-                + "Illegal Information Exchange, messages are not allowed!");
-    }
-    
-    @Test
-    final void testMessageFromCD2toCD1() {
-        //init();
-        fogNodePF = new FogNode("files/Policy_PF.json");
-        deviceA = new IoTDev("files/Contract_D1.json", fogNodePF);
-        deviceB = new IoTDev("files/Contract_D2.json", fogNodePF);
-        
-        assertFalse(deviceB.sendMessage(deviceA, "Hello there!"), "There is an "
-                + "Illegal Information Exchange, messages are allowed!");
-    }
-
-    @Test
-    final void testMessageFromCD2toCD5() {
-        //init();
-        fogNodePFlegal = new FogNode("files/Policy_PFlegal.json");
-        deviceC = new IoTDev("files/Contract_D5.json", fogNodePFlegal);
-        deviceD = new IoTDev("files/Contract_D2.json", fogNodePFlegal);
-        
-        assertTrue(deviceD.sendMessage(deviceC, "Hello there!"), "There is not "
-                + "an Illegal Information Exchange, messages are allowed!");
-    }
-    
-    @Test
-    final void testMessageFromCD5toCD2() {
-        //init();
-        fogNodePFlegal = new FogNode("files/Policy_PFlegal.json");
-        deviceC = new IoTDev("files/Contract_D5.json", fogNodePFlegal);
-        deviceD = new IoTDev("files/Contract_D2.json", fogNodePFlegal);
-        
-        assertTrue(deviceC.sendMessage(deviceD, "Hello there!"), "There is not "
-                + "an Illegal Information Exchange, messages are allowed!");
-    }
-    
-    @Test
     final void testHasNoContract() {
-        //init();
         deviceEmpty = new IoTDev();
         
         assertFalse(deviceEmpty.hasContract(), "We created a default device"
@@ -243,7 +174,6 @@ class IoTDevTest {
     
     @Test
     final void testHasContract() {
-        //init();
         fogNodePF = new FogNode("files/Policy_PF.json");
         deviceA = new IoTDev("files/Contract_D1.json", fogNodePF);
         
